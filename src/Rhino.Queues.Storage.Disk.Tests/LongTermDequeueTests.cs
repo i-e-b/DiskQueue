80ee012cee77b09a2cd6703783a708207f71d88c
Iain Ballard <iain.ballard@7digital.com>
2013-07-17 11:49:22 +0100

    Little test

---
+++ src/Rhino.Queues.Storage.Disk.Tests/LongTermDequeueTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using DiskQueue;
+using NUnit.Framework;
+
+namespace Rhino.Queues.Storage.Disk.Tests
+{
+	[TestFixture]
+	public class LongTermDequeueTests
+	{
+		IPersistentQueue _q;
+
+		[SetUp]
+		public void setup()
+		{
+			_q = PersistentQueue.WaitFor("./queue", TimeSpan.FromSeconds(10));
+		}
+
+		[Test]
+		public void can_enqueue_during_a_long_dequeue ()
+		{
+			var s1 = _q.OpenSession();
+
+			using (var s2 = _q.OpenSession())
+			{
+				s2.Enqueue(new byte[]{1,2,3,4});
+				s2.Flush();
+			}
+
+			var x = s1.Dequeue();
+			s1.Flush();
+			s1.Dispose();
+
+			Assert.That(x.SequenceEqual(new byte[]{1,2,3,4}));
+		}
+
+	}
+}
